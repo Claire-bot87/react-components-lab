111c0e96862d03ab54ab02af810e0bd03feea791
Claire <clairebrady12@hotmail.com>
2025-01-27 17:12:29 +0000

    completed

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,15 @@
+ import './WeatherForecast.css'
+
+const WeatherForecastListItem = (props) => {
+    const {weatherForecast} = props
+    return(
+<div className="weather">
+  <h2>{weatherForecast.day}</h2>
+  <img src={weatherForecast.img} alt={weatherForecast.imgAlt} />
+  <p><span>conditions: </span>{weatherForecast.conditions}</p>
+  <p><span>time: </span>{weatherForecast.time}</p>
+</div>
+)
+}
+
+export default WeatherForecastListItem
